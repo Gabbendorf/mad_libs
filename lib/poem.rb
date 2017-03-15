b979896ce866fd73ef94174b7f31e43395ad99ec
Gabriella <gabriellamds@yahoo.it>
2017-03-15 15:06:16 +0000

    test method 'get_values' of class Poem

--- lib/poem.rb
+++ lib/poem.rb
@@ -2,5 +2,19 @@ require_relative 'ui'
 
 class Poem
 
+  attr_reader :first_noun, :plural_noun, :second_noun, :place, :adjective, :third_noun
+
+  def initialize(ui)
+    @ui = ui
+  end
+
+  def get_values
+    @first_noun = @ui.get_first_noun
+    @plural_noun = @ui.get_plural_noun
+    @second_noun = @ui.get_second_noun
+    @place = @ui.get_place
+    @adjective = @ui.get_adjective
+    @third_noun = @ui.get_third_noun
+  end
 
 end
--- spec/poem_spec.rb
+++ spec/poem_spec.rb
@@ -6,6 +6,22 @@ RSpec.describe Poem do
   let(:poem) {Poem.new(ui)}
   let(:ui) {Ui.new($stdin, $stdout)}
 
-  
+  it "stores all user's inputs in instance variables" do
+    ui = double('ui')
+    expect(ui).to receive(:get_first_noun).and_return("cat")
+    expect(ui).to receive(:get_plural_noun).and_return("dogs")
+    expect(ui).to receive(:get_second_noun).and_return("ice-cream")
+    expect(ui).to receive(:get_place).and_return("New York")
+    expect(ui).to receive(:get_adjective).and_return("yellow")
+    expect(ui).to receive(:get_third_noun).and_return("moon")
+    poem = Poem.new(ui)
+    poem.get_values
+    expect(poem.first_noun).to eq("cat")
+    expect(poem.plural_noun).to eq("dogs")
+    expect(poem.second_noun).to eq("ice-cream")
+    expect(poem.place).to eq("New York")
+    expect(poem.adjective).to eq("yellow")
+    expect(poem.third_noun).to eq("moon")
+  end
 
 end
